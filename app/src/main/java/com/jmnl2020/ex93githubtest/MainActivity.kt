01288ae0a455b5f46282a10b926356cfd88dd018
jmnl <joominl15@gmail.com>
2020-07-03 14:35:24 +0900

    Initial commit

---
+++ app/src/main/java/com/jmnl2020/ex93githubtest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jmnl2020.ex93githubtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
